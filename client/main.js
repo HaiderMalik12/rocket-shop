1444ebf9187504511a266bf943b69290d70120c2
Haider Malik <haidermalik5044@gmail.com>
2016-06-14 00:54:19 +0500

    Create a prototyping Module

---
+++ client/main.js
@@ -0,0 +1,9 @@
+//global helpers
+UI.registerHelper('money', function(amount) {
+    return accounting.formatMoney(amount);
+});
+
+UI.registerHelper('markdown', function(text) {
+    var converter = new showdown.Converter();
+    return converter.makeHtml(text);
+});
